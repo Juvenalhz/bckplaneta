c1ff13bef0a21aef69c1e2756dcdcaef81812a81
Juvenal Hernandez <jjhernandezz100@gmail.com>
2022-04-12 17:46:25 -0400

    ordenes

--- app/controllers/ordenesController.js
+++ app/controllers/ordenesController.js
@@ -6,11 +6,13 @@ const { promisify } = require('util');
 require('dotenv').config({ path: '../../.env' })
 
 exports.queryOrdenes = async (req, res) => { 
+    
 
     const iduser = req.body.iduser
     
 
     conexion.query("SELECT * FROM pedidos where id_user = " + iduser, (err, results) => {
+        
         if (err) {
             console.log(err);
             res.status(500).send('Error al consultar ordenes');
@@ -18,7 +20,7 @@ exports.queryOrdenes = async (req, res) => {
         console.log(results[0])
 
         res.status(200).json({
-            results: results[0]
+            results: results
         })
      } )
 }
